e7797c2da80424dc1b6a12c8d3ffceccd1e5d8e9
Natalie Foster <n_foster_@hotmail.com>
2016-06-01 10:21:47 +0100

    Add files via upload

---
+++ CCDefault.aspx
@@ -0,0 +1,86 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<title> Currency Converter</title>
+</head>
+
+<body style="background-color: Black; font-family:Calibri, Sans-Serif, Arial; color: White ">
+
+    <form id="form1" runat="server">
+
+     
+     <b><asp:Label ID="Label3" runat="server" Text=" Standard Currency Converter"></asp:Label></b>
+       :
+          <div style="height: 346px">
+  
+        <p>Enter a number: <asp:TextBox ID="Txt3" runat="server"></asp:TextBox></p>
+        
+        &nbsp;
+              <p style="text-align: left">
+                  Select Currency: 
+                  <asp:DropDownList ID="DDL3" runat="server" 
+                onselectedindexchanged="DDL3_SelectedIndexChanged" >
+                      <asp:ListItem Value="1.2329">Canadian Dollar</asp:ListItem>
+                      <asp:ListItem Value="0.9326">Swiss Franc</asp:ListItem>
+                      <asp:ListItem Value="0.6432">Pound Sterling</asp:ListItem>
+                      <asp:ListItem Value="1">US Dollar</asp:ListItem>
+                      <asp:ListItem Value="123.454">Japanese Yen</asp:ListItem>
+                  </asp:DropDownList>
+              </p>
+            
+        <p>Select Currency: <asp:DropDownList ID="DDL4" runat="server">
+            <asp:ListItem Value="1.2329">Canadian Dollar</asp:ListItem>
+            <asp:ListItem Value="0.9326">Swiss Franc</asp:ListItem>
+            <asp:ListItem Value="0.6432">Pound Sterling</asp:ListItem>
+            <asp:ListItem Value="1">US Dollar</asp:ListItem>
+            <asp:ListItem Value="123.454">Japanese Yen</asp:ListItem>
+
+        </asp:DropDownList></p>
+            
+     
+          <p><asp:Button ID="Button2" runat="server" Text="Convert Currency" onclick="Button2_Click" />    </p>
+              <p>
+                  <asp:Label ID="Label4" runat="server" Text="Conversion"></asp:Label>
+              </p>   
+          </div>
+     
+        <b><asp:Label ID="Label1" runat="server" Text="Currency Converter using Json API"></asp:Label></b>
+       
+          <div style="height: 346px">
+  
+        <p>Enter a number: <asp:TextBox ID="Txt1" runat="server"></asp:TextBox></p>
+        
+        &nbsp;
+        <p> Select Currency: <asp:DropDownList ID="DDLfrom" runat="server" >
+              
+        <asp:ListItem Value="CAD" Text="Canadian Dollar"></asp:ListItem>
+        <asp:ListItem Value="CHF" Text="Swiss Franc"></asp:ListItem>
+        <asp:ListItem Value="GBP" Text="Pound Sterling"></asp:ListItem>
+        <asp:ListItem Value="USD" Text="US Dollar"></asp:ListItem>
+        <asp:ListItem Value="JPY" Text="Japanese Yen"></asp:ListItem>
+        </asp:DropDownList></p>
+            
+        <p>Select Currency: <asp:DropDownList ID="DDLto" runat="server">
+     
+        <asp:ListItem Value="CAD" Text="Canadian Dollar"></asp:ListItem>
+        <asp:ListItem Value="CHF" Text="Swiss Franc"></asp:ListItem>
+        <asp:ListItem Value="GBP" Text="Pound Sterling"></asp:ListItem>
+        <asp:ListItem Value="USD" Text="US Dollar"></asp:ListItem>
+        <asp:ListItem Value="JPY" Text="Japanese Yen"></asp:ListItem>
+        </asp:DropDownList></p>
+
+        
+        <asp:Button ID="Button1" runat="server" Text="Convert Currency" 
+                  onclick="Button1_Click" style="height: 26px" />
+       
+      
+        <br />
+       
+    </div>
+
+    </form>
+</body>
+</html>
---
+++ CCDefault.aspx.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.Script.Serialization;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.IO;
+
+
+
+public partial class _Default : System.Web.UI.Page
+{
+
+    //CURRENCY CONVERTER USING JSON API
+    public class Rate
+    {
+        //read and write properties
+
+        public string to { get; set; } //paired dataset 
+
+        public string from { get; set; }
+
+        public double rate { get; set; } // Time stamp and base are objects that could alo be extracted but not required for the programme
+
+
+     }
+
+    //This is a first attempt at using asp.Net, the comments will explain my logic for the code. However, even though the database it recognised in the system extraction to the currencies was not achieved
+
+    protected void Button1_Click(object sender, EventArgs e )// when conversion buttion is press the following should take place
+    {
+
+        double amount; // amount entered by the user
+
+        if (double.TryParse(Txt1.Text, out amount)) // amount entered in first text box converted to double output
+        {
+
+            WebClient web = new WebClient(); //webclient to download file
+            
+            string url = string.Format("http://api.fixer.io/latest?from={0}&to={1}", DDLfrom.SelectedItem.Value, DDLto.SelectedItem.Value); //bind json database and set drop down list parameter
+
+            string rates = web.DownloadString(url); //extract rates
+
+            Rate rate = new JavaScriptSerializer().Deserialize<Rate>(rates);  //deserialise JSON file
+
+            double exchange = amount * rate.rate; //calculation for currency conversion
+                     
+            string message =  amount+ "  " + DDLfrom.SelectedItem.Text + "\\n"; // show about enter with name of currency chosen
+
+            message += "The exchange rate for: " + DDLfrom.SelectedItem.Text + " to " + DDLto.SelectedItem.Text + " = " + exchange + "\\n"; // show exchange rate in message
+
+            ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('" + message + "');", true);
+
+        }
+
+        else
+        {
+
+            ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('Invalid amount value.');", true);
+
+        }
+
+    }
+    protected void DDL3_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+
+        //STANDARD CURRENCY CONVERTER 
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        int amount; // amount to be entered by the user
+       
+        int.TryParse(Txt3.Text, out amount); //text box converted to interget input
+       
+
+        double currency1 = double.Parse(DDL3.SelectedItem.Value); //currency variable to converted value from dropdown list
+        double currency2 = double.Parse(DDL4.SelectedItem.Value);
+        double conversion; 
+        double con = amount * currency1;//conversion for first inout for first seleted currency
+       
+       
+        // if statements to produce error statement if the same currency is selected
+
+        if (DDL3.SelectedValue == "1.2329" && DDL4.SelectedValue == "1.2329")
+        {
+
+            Label4.Text = "INCORRECT SELECTION";
+        }
+        else if (DDL3.SelectedValue == "0.9326" && DDL4.SelectedValue == "0.9326")
+        {
+
+            Label4.Text = "INCORRECT SELECTION";
+        }
+        else if (DDL3.SelectedValue == "0.6432" && DDL4.SelectedValue == "0.6432")
+        {
+
+
+            Label4.Text = "INCORRECT SELECTION";
+        }
+
+        else if (DDL3.SelectedValue == "1" && DDL4.SelectedValue == "1")
+        {
+
+            Label4.Text = "INCORRECT SELECTION";
+        }
+
+        else if (DDL3.SelectedValue == "123.454" && DDL4.SelectedValue == "123.454")
+        {
+
+            Label4.Text = "INCORRECT SELECTION";
+
+            //Canadian Dollar Exhange
+        }
+        else if (DDL3.SelectedValue == "1.2329" && DDL4.SelectedValue == "0.9326") // when different selection are made from each comboBox a calculation for that currency will be performed
+        {
+            conversion = con * currency2; // currency is user input amount multiplied by the exchange rate
+            Label4.Text = "The exchange rate is: " + DDL4.SelectedItem.Text + " " +conversion.ToString("N2"); // label 1 text, will display user input amount and name of current in comboBox 1 index selected as 0
+            
+        }
+
+        else if (DDL3.SelectedValue == "1.2329" && DDL4.SelectedValue == "0.6432") {
+           conversion = con * currency2;
+           Label4.Text = "The exchange rate is: " + DDL4.SelectedItem.Text + " " + conversion.ToString("N2"); 
+
+        }
+
+        else if (DDL3.SelectedValue == "1.2329" && DDL4.SelectedValue == "1") {
+            conversion = con * currency2;
+            Label4.Text = "The exchange rate is: " + DDL4.SelectedItem.Text + " " + conversion.ToString("N2"); 
+
+        }
+
+        else if (DDL3.SelectedValue == "1.2329" && DDL4.SelectedValue == "123.454") {
+           conversion = con * currency2;
+           Label4.Text = "The exchange rate is: " + DDL4.SelectedItem.Text + " " + conversion.ToString("N2");
+        }
+
+            //Swiss France Exchange
+
+        else if (DDL3.SelectedValue == "0.9326" && DDL4.SelectedValue == "1.2329") {
+            conversion = con * currency2;
+            Label4.Text = "The exchange rate is: " + DDL4.SelectedItem.Text + " " + conversion.ToString("N2"); 
+
+        }
+
+        else if (DDL3.SelectedValue == "0.9326" && DDL4.SelectedValue == "0.6432") {
+            conversion = con * currency2;
+            Label4.Text = "The exchange rate is: " + DDL4.SelectedItem.Text + " " + conversion.ToString("N2"); 
+
+        }
+
+        else if (DDL3.SelectedValue == "0.9326" && DDL4.SelectedValue == "1") {
+            conversion = con * currency2;
+            Label4.Text = "The exchange rate is: " + DDL4.SelectedItem.Text + " " + conversion.ToString("N2"); 
+
+        }
+
+        else if (DDL3.SelectedValue == "0.9326" && DDL4.SelectedValue == "123.454") {
+            conversion = con * currency2;
+            Label4.Text = "The exchange rate is: " + DDL4.SelectedItem.Text + " " + conversion.ToString("N2"); 
+
+        }
+
+         //Pound Sterling Exchange
+
+        else if (DDL3.SelectedValue == "0.6432" && DDL4.SelectedValue == "1.2329") {
+           conversion = con * currency2;
+           Label4.Text = "The exchange rate is: " + DDL4.SelectedItem.Text + " " + conversion.ToString("N2"); 
+        }
+
+        else if (DDL3.SelectedValue == "0.6432" && DDL4.SelectedValue == "0.9326") {
+           conversion = con * currency2;
+           Label4.Text = "The exchange rate is: " + DDL4.SelectedItem.Text + " " + conversion.ToString("N2"); 
+
+        }
+
+        else if (DDL3.SelectedValue == "0.6432" && DDL4.SelectedValue == "1") {
+            conversion = con * currency2;
+            Label4.Text = "The exchange rate is: " + DDL4.SelectedItem.Text + " " + conversion.ToString("N2"); 
+
+        }
+
+        else if (DDL3.SelectedValue == "0.6432" && DDL4.SelectedValue == "123.454") {
+          conversion = con * currency2;
+          Label4.Text = "The exchange rate is: " + DDL4.SelectedItem.Text + " " + conversion.ToString("N2"); 
+
+        }
+        //US Dollar
+
+        else if (DDL3.SelectedValue == "1" && DDL4.SelectedValue == "1.2329") {
+            conversion = con * currency2;
+            Label4.Text = "The exchange rate is: " + DDL4.SelectedItem.Text + " " + conversion.ToString("N2"); 
+
+        }
+
+        else if (DDL3.SelectedValue == "1" && DDL4.SelectedValue == "0.9326") {
+           conversion = con * currency2;
+           Label4.Text = "The exchange rate is: " + DDL4.SelectedItem.Text + " " + conversion.ToString("N2"); 
+
+        }
+
+        else if (DDL3.SelectedValue == "1" && DDL4.SelectedValue == "0.6432") {
+            conversion= con * currency2;
+            Label4.Text = "The exchange rate is: " + DDL4.SelectedItem.Text + " " + conversion.ToString("N2"); 
+
+        }
+
+        else if (DDL3.SelectedValue == "1" && DDL4.SelectedValue == "123.454") {
+            conversion = con * currency2;
+            Label4.Text = "The exchange rate is: " + DDL4.SelectedItem.Text + " " + conversion.ToString("N2"); 
+
+        }
+        //Japanese Yen
+
+
+        else if (DDL3.SelectedValue == "123.454" && DDL4.SelectedValue == "1.2329") {
+            conversion = con * currency2;
+            Label4.Text = "The exchange rate is: " + DDL4.SelectedItem.Text + " " + conversion.ToString("N2"); 
+
+        }
+
+        else if (DDL3.SelectedValue == "123.454" && DDL4.SelectedValue == "0.9326") 
+        {
+            conversion = con * currency2;
+            Label4.Text = "The exchange rate is: " + DDL4.SelectedItem.Text + " " + conversion.ToString("N2"); 
+
+        }
+
+        else if (DDL3.SelectedValue == "123.454" && DDL4.SelectedValue == "0.6432") 
+        {
+            conversion = con * currency2;
+            Label4.Text = "The exchange rate is: " + DDL4.SelectedItem.Text + " " + conversion.ToString("N2"); 
+
+        }
+
+        else if (DDL3.SelectedValue == "123.454" && DDL4.SelectedValue == "1")         {
+            conversion = con * currency2;
+            Label4.Text = "The exchange rate is: " + DDL4.SelectedItem.Text + " " + conversion.ToString("N2"); 
+
+        }
+    }}
+
+         
